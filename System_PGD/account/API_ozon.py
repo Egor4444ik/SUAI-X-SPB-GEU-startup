4afffa20dad134e26360bbf342ed6a794f30b8af
Egor4ik <egor4e4ik@gmail.com>
2024-11-29 12:35:36 +0300

    fix dynamic model

--- System_PGD/account/API_ozon.py
+++ System_PGD/account/API_ozon.py
@@ -1,56 +1,71 @@
 import requests, json
 
+url = 'https://api-seller.ozon.ru'
+
 client_id = '2423263'
 api_key = 'ada677c3-3558-44e5-bb41-d3ed8ad25c15'
 
-url = 'https://api-seller.ozon.ru'
-
-def count_of_goods(method_of_stock='', offer_id='', product_id='', identify_of_stock=None, count_of_stocks=None):
-    if method_of_stock == 'FBO':
-        url_to_count_of_goods = url + '/v3/product/info/stocks'
+def goods_info(take_price = False, method_of_stock='', offer_id='', product_id='', identify_of_stock=None, count_of_stocks=None):
+    if take_price:
+        url_to_info = url + '/v4/product/info/prices'
         data = {
             "filter": {
-                "offer_id": [offer_id] if offer_id else [], #Обработка пустого offer_id
-                "product_id": [product_id] if product_id else [], #Обработка пустого product_id
+                "offer_id": offer_id if offer_id else '0',  # Обработка пустого offer_id
+                "product_id": product_id if product_id else 0,  # Обработка пустого product_id
+                "visibility": "ALL"
+            },
+            "last_id": "",
+            "limit": 100
+        }
+        return {'url': url_to_info, 'data': data}
+
+    if method_of_stock == 'FBO':
+        url_to_info = url + '/v3/product/info/stocks'
+        data = {
+            "filter": {
+                "offer_id": offer_id if offer_id else '0', #Обработка пустого offer_id
+                "product_id": product_id if product_id else 0, #Обработка пустого product_id
                 "visibility": "ALL"
             },
                 "last_id": "",
                 "limit": 100
         }
     elif method_of_stock in ('FBS', 'rFBS'):
-        url_to_count_of_goods = url + '/v1/product/info/stocks-by-warehouse/fbs'
+        url_to_info = url + '/v1/product/info/stocks-by-warehouse/fbs'
+        print('offer_id', offer_id)
+        print('product_id', product_id)
         data = {
             "filter": {
-                "offer_id": [offer_id] if offer_id else [], #Обработка пустого offer_id
-                "product_id": [product_id] if product_id else [], #Обработка пустого product_id
+                "offer_id": offer_id if offer_id else '0', #Обработка пустого offer_id
+                "product_id": product_id if product_id else 0, #Обработка пустого product_id
                 "visibility": "ALL",
-                "sku": [""] #Заглушка, если sku неизвестен
+                "sku": "123456" #Заглушка, если sku неизвестен
               },
             "last_id": "",
             "limit": 100
         }
     else:
-        if count_of_stocks: # Проверка на None или пустую строку
-            url_to_count_of_goods = url + '/v1/product/import/stocks'
-            data = {"count_of_stocks": count_of_stocks} #Заполняем count_of_stocks
-        elif identify_of_stock: # Проверка на None или пустую строку
-            url_to_count_of_goods = url + '/v2/products/stocks' + identify_of_stock
-            data = {} # Пустые данные
+        if count_of_stocks:
+            url_to_info = url + '/v1/product/import/stocks'
+            data = {"count_of_stocks": count_of_stocks}
+        elif identify_of_stock:
+            url_to_info = url + '/v2/products/stocks/' + str(identify_of_stock)
+            data = {}
         else:
-            return None #Возвращаем None, если данные некорректны
+            print('Данные некорректны')
+            return None
 
 
-    return {'url': url_to_count_of_goods, 'data': data}
+    return {'url': url_to_info, 'data': data}
 
 
-
-def take_fullfilment(Client_Id='', Api_key='', Stock_method='', offer_id='', product_id='', identify_of_stock=None, count_of_stocks=None):
+def take_info(Client_Id='', Api_key='', take_price=False, Stock_method='', offer_id='', product_id='', identify_of_stock=None, count_of_stocks=None):
     headers = {
         "Client-Id": Client_Id,
         "Api-Key": Api_key,
         "Content-Type": "application/json"
     }
-    request_data = count_of_goods(method_of_stock=Stock_method, offer_id=offer_id, product_id=product_id,
+    request_data = goods_info(take_price=take_price,method_of_stock=Stock_method, offer_id=offer_id, product_id=product_id,
                  identify_of_stock=identify_of_stock, count_of_stocks=count_of_stocks)
 
     if request_data is None:
@@ -61,7 +76,8 @@ def take_fullfilment(Client_Id='', Api_key='', Stock_method='', offer_id='', pro
         response.raise_for_status() # Поднимает исключение для не-2xx кодов ответа
 
         data = response.json()
-        return data['result']['total']
+        print('Всё норм')
+        return data['result']
 
     except requests.exceptions.HTTPError as e:
         print(f"Ошибка API Ozon (HTTPError): {e}, код ошибки: {response.status_code}, ответ: {response.text}")
@@ -73,4 +89,4 @@ def take_fullfilment(Client_Id='', Api_key='', Stock_method='', offer_id='', pro
         print(f"Непредвиденная ошибка: {e}")
         return None # Возвращаем None при ошибке
 
-print(take_fullfilment(Client_Id=client_id, Api_key=api_key, Stock_method='FBO', offer_id = '123456', product_id = '123456', identify_of_stock = None, count_of_stocks = None))
+print(take_info(Client_Id=client_id, Api_key=api_key, Stock_method='FBS', offer_id = '123456', product_id = 123456))
--- System_PGD/account/models.py
+++ System_PGD/account/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
-from .API_ozon import take_fullfilment, count_of_goods
+from .API_ozon import take_info, goods_info
+import time
 
 class user_data(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -11,7 +12,7 @@ class user_data(models.Model):
     api_key_ozon = models.CharField(blank=True, max_length=36)
     client_id_ozon = models.CharField(blank=True, max_length=20)
     identify_of_stock = models.CharField(blank=True, max_length=20)
-    count_of_stocks = models.CharField(blank=True, max_length=20)
+    count_of_stocks = models.IntegerField(blank=True)
 
     def __str__(self):
         return f'Profile of {self.user.username}'
@@ -23,13 +24,18 @@ class ozon(models.Model):
 
     def update_data(self):
         ozon_goods_cards = ['iphone_16', 'iphone_16_pro_max']
-        all_data = {}
+
+        if self.data != {}:
+            all_data = self.data
+
+        else:
+            all_data = {}
 
         for goods_card in ozon_goods_cards:
             try:
-                attrs = self.get_ozon_data(goods_card)
+                attrs = self.get_ozon_data(all_data, goods_card)
                 if attrs:
-                  all_data[goods_card] = attrs
+                    all_data[goods_card] = attrs
             except Exception as e:
                 print(f"Ошибка при получении данных для {goods_card}: {e}")
 
@@ -37,35 +43,46 @@ class ozon(models.Model):
         self.save()
         print('data has been updated')
 
-    def get_ozon_data(self, goods_card):
+    def get_ozon_data(self, all_data, goods_card):
         user_data_instance = getattr(self.user, 'user_data', None)
         if not user_data_instance:
             print("user_data not found for this user")
             return None
 
         try:
-            #Обработка ошибок 403 непосредственно внутри take_fullfilment, если это возможно.
-            client_id=user_data_instance.client_id_ozon
-            api_key=user_data_instance.api_key_ozon
-            fbs_data = take_fullfilment(Client_Id=client_id, Api_key=api_key, Stock_method='FBS', offer_id=offer_id, product_id=product_id)
-            fbo_data = take_fullfilment(Client_Id=client_id, Api_key=api_key, Stock_method='FBO', offer_id=offer_id, product_id=product_id)
-            other_fullfilment_data = take_fullfilment(Client_Id=client_id, Api_key=api_key, count_of_stocks=user_data_instance.count_of_stocks,
-                                          identify_of_stock=user_data_instance.identify_of_stock, offer_id=offer_id, product_id=product_id)
+            client_id = user_data_instance.client_id_ozon
+            api_key = user_data_instance.api_key_ozon
+
+            fbs_data = take_info(Client_Id=client_id, Api_key=api_key, Stock_method='FBS', offer_id='123456', product_id='123456')
+            fbo_data = take_info(Client_Id=client_id, Api_key=api_key, Stock_method='FBO', offer_id='123456', product_id='123456')
+            other_fullfilment_data = [take_info(Client_Id=client_id, Api_key=api_key, count_of_stocks=int(user_data_instance.count_of_stocks),
+                                               identify_of_stock=int(user_data_instance.identify_of_stock),
+                                               offer_id='123456', product_id='123456')]
+            date = [time.strftime("%Y-%m-%d")]
+            price = [take_info(Client_Id=client_id, Api_key=api_key, take_price='True', offer_id='123456',
+                              product_id='123456')]
+
+            fbo = [all_data[goods_card]['count_from_FBO']] + fbo_data['items']
+            fbs = [all_data[goods_card]['count_from_FBS']] + [fbs_data]
+            print(fbo, fbs)
             attrs = {
-              'category': 'Phone',
-              'offer_id': '123456',
-              'product_id': '123456',
-              'count_from_FBS': str(fbs_data),
-              'count_from_FBO': str(fbo_data),
-              'count_from_other_fullfilment': str(other_fullfilment_data),
-              'buyer_name': 'Some Buyer',
-              'buyer_sex': 'Male',
-              'buyer_age': '30',
-              'buyer_region': 'Moscow',
-              'buyer_num_session': '5',
-              'buyer_avg_session': '15',
-              'date': '2024-10-27',
+                'category': 'Tech',
+                'undercategory': 'Iphone',
+                'underundercategory': 'Iphone_new_gen',
+                'offer_id': '123456',
+                'product_id': '123456',
+                'count_from_FBS': fbs,
+                'count_from_FBO': fbo,
+                'count_from_other_fullfilment': [all_data[goods_card]['other_fullfilment_data']] + [other_fullfilment_data],
+                'buyer_name': 'Some Buyer',
+                'buyer_sex': 'Male',
+                'buyer_age': '30',
+                'buyer_region': 'Moscow',
+                'buyer_num_session': '5',
+                'date': [all_data[goods_card]['date']] + [date],
+                'price': [all_data[goods_card]['price']] + [price]
             }
+
             return attrs
         except Exception as e:
             print(f"Ошибка API Ozon: {e}")
