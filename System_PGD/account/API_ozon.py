7e3c646b4724a9e9bbf0924a34b7ccebcffb0a91
MARXstoodin <108128873+MARXstoodin@users.noreply.github.com>
2024-12-22 10:56:06 +0300

    Add files via upload

--- System_PGD/account/API_ozon.py
+++ System_PGD/account/API_ozon.py
@@ -1,17 +1,23 @@
-import requests, json
+import requests, json, logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def present_and_previous_data(old_data, offer_id, product_id):
+    present_data = take_info(Client_Id=client_id, Api_key=api_key, take_price='True', offer_id=offer_id, product_id=product_id)
+    previous_data = old_data['iphone_16']['price']
+    return ([previous_data] if type(previous_data) != list else previous_data) + [present_data]
 
 url = 'https://api-seller.ozon.ru'
-
 client_id = '2423263'
 api_key = 'ada677c3-3558-44e5-bb41-d3ed8ad25c15'
 
-def goods_info(take_price = False, method_of_stock='', offer_id='', product_id='', identify_of_stock=None, count_of_stocks=None):
+def goods_info(take_price = False, method_of_stock='', offer_id='', product_id='', sku = "", identify_of_stock=None, count_of_stocks=None):
     if take_price:
         url_to_info = url + '/v4/product/info/prices'
         data = {
             "filter": {
-                "offer_id": offer_id if offer_id else '0',  # Обработка пустого offer_id
-                "product_id": product_id if product_id else 0,  # Обработка пустого product_id
+                "offer_id": [offer_id] if offer_id else [],  # Обработка пустого offer_id
+                "product_id": [product_id] if product_id else [],  # Обработка пустого product_id
                 "visibility": "ALL"
             },
             "last_id": "",
@@ -23,8 +29,8 @@ def goods_info(take_price = False, method_of_stock='', offer_id='', product_id='
         url_to_info = url + '/v3/product/info/stocks'
         data = {
             "filter": {
-                "offer_id": offer_id if offer_id else '0', #Обработка пустого offer_id
-                "product_id": product_id if product_id else 0, #Обработка пустого product_id
+                "offer_id": [offer_id] if offer_id else [], #Обработка пустого offer_id
+                "product_id": [product_id] if product_id else [], #Обработка пустого product_id
                 "visibility": "ALL"
             },
                 "last_id": "",
@@ -32,20 +38,13 @@ def goods_info(take_price = False, method_of_stock='', offer_id='', product_id='
         }
     elif method_of_stock in ('FBS', 'rFBS'):
         url_to_info = url + '/v1/product/info/stocks-by-warehouse/fbs'
-        print('offer_id', offer_id)
-        print('product_id', product_id)
         data = {
-            "filter": {
-                "offer_id": offer_id if offer_id else '0', #Обработка пустого offer_id
-                "product_id": product_id if product_id else 0, #Обработка пустого product_id
-                "visibility": "ALL",
-                "sku": "123456" #Заглушка, если sku неизвестен
-              },
-            "last_id": "",
-            "limit": 100
+                "sku": [
+                    sku
+                ]
         }
     else:
-        if count_of_stocks:
+        if count_of_stocks == 1:
             url_to_info = url + '/v1/product/import/stocks'
             data = {"count_of_stocks": count_of_stocks}
         elif identify_of_stock:
@@ -55,38 +54,40 @@ def goods_info(take_price = False, method_of_stock='', offer_id='', product_id='
             print('Данные некорректны')
             return None
 
-
     return {'url': url_to_info, 'data': data}
 
 
-def take_info(Client_Id='', Api_key='', take_price=False, Stock_method='', offer_id='', product_id='', identify_of_stock=None, count_of_stocks=None):
+def take_info(Client_Id='', Api_key='', take_price=False, Stock_method='', offer_id='', product_id='', sku='', identify_of_stock=None, count_of_stocks=None):
     headers = {
         "Client-Id": Client_Id,
         "Api-Key": Api_key,
         "Content-Type": "application/json"
     }
-    request_data = goods_info(take_price=take_price,method_of_stock=Stock_method, offer_id=offer_id, product_id=product_id,
-                 identify_of_stock=identify_of_stock, count_of_stocks=count_of_stocks)
+    request_data = goods_info(take_price=take_price, method_of_stock=Stock_method, offer_id=offer_id, product_id=product_id, sku=sku,
+                              identify_of_stock=identify_of_stock, count_of_stocks=count_of_stocks)
 
     if request_data is None:
-        return None # Данные некорректны
+        return None  # Данные некорректны
+
+    #logging.debug(f"Запрос к API: URL={request_data['url']}, Данные={json.dumps(request_data['data'], indent = 2)}, Заголовки={headers}") # логируем запрос
 
     try:
         response = requests.post(request_data['url'], headers=headers, json=request_data['data'])
-        response.raise_for_status() # Поднимает исключение для не-2xx кодов ответа
+        response.raise_for_status()  # Поднимает исключение для не-2xx кодов ответа
 
         data = response.json()
-        print('Всё норм')
+        #logging.debug(f"Ответ API: {data}") # логируем ответ
         return data['result']
 
     except requests.exceptions.HTTPError as e:
-        print(f"Ошибка API Ozon (HTTPError): {e}, код ошибки: {response.status_code}, ответ: {response.text}")
-        return None # Возвращаем None при ошибке
+        #logging.error(f"Ошибка API Ozon (HTTPError): {e}, код ошибки: {response.status_code}, ответ: {response.text}")
+        return None  # Возвращаем None при ошибке
     except (KeyError, json.JSONDecodeError) as e:
-        print(f"Ошибка обработки ответа API Ozon: {e}, ответ: {response.text}")
-        return None # Возвращаем None при ошибке
+        #logging.error(f"Ошибка обработки ответа API Ozon: {e}, ответ: {response.text}")
+        return None  # Возвращаем None при ошибке
     except Exception as e:
-        print(f"Непредвиденная ошибка: {e}")
-        return None # Возвращаем None при ошибке
+        #logging.exception(f"Непредвиденная ошибка: {e}")  # логируем исключения с traceback
+        return None  # Возвращаем None при ошибке
 
-print(take_info(Client_Id=client_id, Api_key=api_key, Stock_method='FBS', offer_id = '123456', product_id = 123456))
+
+print(take_info(Client_Id=client_id, Api_key=api_key, Stock_method='FBS', offer_id='123456', product_id='123456', sku='654321'))
